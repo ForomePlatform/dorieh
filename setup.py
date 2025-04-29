89ac83eb79fe1f314ce145c18e9da63ca19f8732
mmcentre <michael.bouzinier@kvitneset.com>
2025-04-29 01:45:43 -0400

    MAB   Aggregating 2024 FOSSA data

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name='dorieh',
-    version="0.3.2",
+    version="0.3.3",
     url='https://github.com/NSAPH-Data-Platform/dorieh',
     license='Apache 2.0',
     author='Michael A Bouzinier',
